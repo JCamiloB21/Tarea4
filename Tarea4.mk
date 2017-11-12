2c0a16b4e198796febbef409df02d5ad18b45085
JCamiloB21 <32931153+JCamiloB21@users.noreply.github.com>
2017-11-11 22:21:29 -0500

    Tarea4.mk

--- Tarea4.mk
+++ Tarea4.mk
@@ -1,3 +1,8 @@
+all: Resultados_hw4.pdf sonido.wav
+sonido.wav: Plots.py audio.dat
+    python Plots.py
+audio.dat: Ondas.c
+	gcc -lm Ondas.c
 Resultados_hw4.pdf : Resultados_hw4.tex graficas.png
 	pdflatex $< 
 graficas.png : cuerda.tex tambor.tex Plots.py
@@ -6,5 +11,4 @@ cuerda.tex : Ondas.c
 	gcc -lm Ondas.c
 tambor.tex : Ondas.c
 	gcc -lm Ondas.c
-clean :
-	rm cuerda.tex graficas.png tambor.tex 
+
